97bde12f2bb76d752cd794f0d24e382e8099b945
kim keesik <wizardseye@trycatchmedia.com>
2017-08-07 16:58:21 +0900

    level editor 기본 소스 틀 올림.

---
+++ src/components/chapters/ChaptersComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChaptersComponent extends Component {
+  render() {
+    return (
+    <div>
+      <h1> 여기는 챕터 컴포넌트 </h1>
+    </div>
+    );
+  }
+}
+
+export default ChaptersComponent;
---
+++ src/components/level/LevelComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LevelComponent extends Component {
+  render() {
+    return (
+    <div>
+      <h1> 여기는 레벨 컴포넌트 </h1>
+    </div>
+    );
+  }
+}
+
+export default LevelComponent;
